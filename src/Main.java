fc92b48c40b52282c8ee2c025b2b6c59d8259861
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-05-25 15:04:15 +0700

    25/5/2020

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailExample emailExample =new EmailExample();
+        System.out.println(emailExample.validate("a@abcc"));
+    }
+}
